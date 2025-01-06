1cbc9520d333c083ce9eaca15729aa95b122c4b8
JMY1993 <yangmohan1993@163.com>
2025-01-06 12:54:15 +0800

    lsp config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,10 +4,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.clipboard="unnamedplus"
+vim.cmd("set number")
 
 if vim.g.neovide then
   local t = 0.9
-  vim.g.neovide_transparency = t 
-  vim.g.neovide_normal_opacity = t 
+  vim.g.neovide_transparency = t
+  vim.g.neovide_normal_opacity = t
 end
 
